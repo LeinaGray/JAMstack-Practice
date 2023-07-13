aa6e589067bf4ceb74b6d973b90703a1ed2a4f80
Leina Gray <abad.alleina@gmail.com>
2023-07-14 00:36:00 +0800

    Template for menu page

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
 module.exports = function(eleventyConfig) {
     return {
       dir: {
-        input: "src"
+        input: "src",
+        output: "site",
+        // data: "menu",
+//         includes: "_includes",
+//         // layouts: "src/layouts",
       }
     }
   };
